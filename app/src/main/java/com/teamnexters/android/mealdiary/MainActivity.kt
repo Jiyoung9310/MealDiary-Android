2244dd35875bd02e6b789cdb61fe2d19fc72b023
Jaehwan <ojh102@gmail.com>
2019-01-01 16:02:35 +0900

    설정 빠진거 추가

--- app/src/main/java/com/teamnexters/android/mealdiary/MainActivity.kt
+++ app/src/main/java/com/teamnexters/android/mealdiary/MainActivity.kt
@@ -2,11 +2,18 @@ package com.teamnexters.android.mealdiary
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Fabric.with(this, Crashlytics())
+
         setContentView(R.layout.activity_main)
     }
 }
